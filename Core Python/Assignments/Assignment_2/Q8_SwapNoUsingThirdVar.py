c7993eb86e88b991f597c0123414a099b3573c15
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 12:28:58 +0530

    Q8_SwapNoUsingThirdVar.py

--- Core Python/Assignments/Assignment_2/Q8_SwapNoUsingThirdVar.py
+++ Core Python/Assignments/Assignment_2/Q8_SwapNoUsingThirdVar.py
@@ -1,14 +1,16 @@
 # 8. WAP to swap two numbers using third variable.
 
-num1 = int(input("Enter No1 = "))
-num2 = int(input("Enter No2 = "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-print("Num1 =",num1)
-print("Num2 =",num2)
+print("Before Swapping")
+print("First Number=",num1)
+print("Second Number=",num2)
 
-temp = num1
+temp = num1 # Third Variable 
 num1 = num2
 num2 = temp
 
-print("Num1 =",num1)
-print("Num2 =",num2)
+print("\nAfter Swapping")
+print("First Number=",num1)
+print("Second Number=",num2)
