9fcab271d84e7dbf711786f84b598465acb2376d
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 14:32:02 +0530

    Q4_Tri&RectArea.py

--- Core Python/Assignments/Assignment_2/Q4_Tri&RectArea.py
+++ Core Python/Assignments/Assignment_2/Q4_Tri&RectArea.py
@@ -1,7 +1,7 @@
 # 4. WAP to calculate area of triangle and rectangle
 # Formula: Traingle= 1/2(b*h), Rectangle= l*b
 
-heigth = float(input("Enter Height for Triangle: "))
+height = float(input("Enter Height for Triangle: "))
 base = float(input("Enter Base for Triangle: "))
 area_t = ((1/2)*(base*heigth))
 print("Area of Triangle is", area_t)
@@ -9,4 +9,5 @@ print("Area of Triangle is", area_t)
 length = float(input("Enter Length for Reactangle: "))
 breadth = float(input("Enter Breadth for Reactangle: "))
 area_r = length*breadth
+
 print("Area of Rectangle is", area_r)
