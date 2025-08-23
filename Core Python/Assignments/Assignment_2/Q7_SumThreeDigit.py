5e1bbb640e948aded0dd204d553ba66e9cc377ed
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 13:01:05 +0530

    Q7_SumThreeDigit.py

--- Core Python/Assignments/Assignment_2/Q7_SumThreeDigit.py
+++ Core Python/Assignments/Assignment_2/Q7_SumThreeDigit.py
@@ -1,6 +1,6 @@
 # 7. Find the sum of three-digit number.
 
-num = int(input('Enter three digit number: '))
+num = int(input('Enter three-digit number: '))
 # 456
 d1 = num % 10    # 456%10= 6
 num = num // 10  # 456//10= 45
@@ -14,3 +14,14 @@ print(f'Digit 1: {d1}, Digit 2: {d2}, Digit 3: {d3}')
 
 sum = d1+d2+d3
 print("Sum of Three Digit number is", sum)
+
+
+# Second approach
+num = int(input("Enter a three-digit number: "))
+
+d1 = num // 100        # hundreds place
+d2 = (num // 10) % 10  # tens place
+d3 = num % 10          # ones place
+
+sum = d1 + d2 + d3
+print("Sum of digits:", sum)
