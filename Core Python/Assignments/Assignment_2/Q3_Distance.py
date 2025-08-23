8f22cc1e5c684500a46ea971594a5df439999c7a
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 12:21:05 +0530

    Q3_Distance.py

--- Core Python/Assignments/Assignment_2/Q3_Distance.py
+++ Core Python/Assignments/Assignment_2/Q3_Distance.py
@@ -1,10 +1,14 @@
 # 3. Convert distant given in feet and inches into meter and centimeter.
+# 1 feet = 0.3048 meter
+# 1 inch = 0.0254 meter
+# 1 meter = 100 cm
 
 feet = float(input("Enter Feet: "))
 inches = float(input("Enter Inches: "))
 
-metre = feet*0.3048
-centimetre = inches*2.54
+# convert feet and inches both into meters 
+m = feet * 0.3048 + inches * 0.0254
+cm = m * 100
 
-print("Feet into Metres: ",metre)
-print("Inches into Centimetre: ",centimetre)
+print(f"Total distance in Metres: {m}")
+print(f"Total distance in Centimetres: {cm}")
