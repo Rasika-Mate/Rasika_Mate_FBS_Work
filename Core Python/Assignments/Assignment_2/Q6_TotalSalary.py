755ead10a8d16f3d876a6be747570b915278b7c3
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 12:54:55 +0530

    Q6_TotalSalary.py

--- Core Python/Assignments/Assignment_2/Q6_TotalSalary.py
+++ Core Python/Assignments/Assignment_2/Q6_TotalSalary.py
@@ -1,10 +1,10 @@
 # 6. WAP to calculate total salary of employee based on basic, da-10% of basic. ta-12% of basic, hra 15% of basic.
 
-basicSalary = float(input("Enter Your Basic Salary: "))
+bs = float(input("Enter Basic Salary: "))
 
-da = ((10/100)*basicSalary)
-ta = ((12/100)*basicSalary)
-hra = ((15/100)*basicSalary)
+da = 0.10*bs # 10% = (10/100) 
+ta = 0.12*bs # 12% = (12/100)
+hra = 0.15*bs # 15% = (15/100)
 
-totalSalary = basicSalary + da + ta + hra
-print("Total salary is",totalSalary)
+totalSalary = bs+da+ta+hra
+print("Total Salary of Employee is", totalSalary)
