65d5eabc1cc7722f1e90e037e15390c4fafba55f
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 12:56:41 +0530

    Q7_SumThreeDigit.py

--- Core Python/Assignments/Assignment_2/Q7_SumThreeDigit.py
+++ Core Python/Assignments/Assignment_2/Q7_SumThreeDigit.py
@@ -10,6 +10,7 @@ num = num // 10  # 45//10= 4
 
 d3 = num % 10    # 4%10= 4
 num = num // 10  # 4//10= 0
-
 print(f'Digit 1: {d1}, Digit 2: {d2}, Digit 3: {d3}')
-print(f"Sum of Three Digit number is {d1 + d2 + d3} ")
+
+sum = d1+d2+d3
+print("Sum of Three Digit number is", sum)
