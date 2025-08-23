7354dc15eeba5a2583ab6084089c067c1266f55d
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-23 12:35:37 +0530

    Q9_SwapNoWithoutUsingThirdVar.py

--- Core Python/Assignments/Assignment_2/Q9_SwapNoWithoutUsingThirdVar.py
+++ Core Python/Assignments/Assignment_2/Q9_SwapNoWithoutUsingThirdVar.py
@@ -1,33 +1,31 @@
 # 9. WAP to swap two numbers without using third variable.
 
 # First Approach
-x = 10
-y = 20
-print(f'Before Swapping - x: {x}, y:{y}')
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(f'\nBefore Swapping - x: {x}, y:{y}')
 
 x,y = y,x
 print(f'After Swapping - x: {x}, y:{y}')
-num1 = int(input("Enter No1 = "))
-num2 = int(input("Enter No2 = "))
+
 
 # Second Approach
-print("Num1 =",num1)
-print("Num2 =",num2)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f'Before Swapping - num1: {num1}, num2:{num2}')
 
-print("# First Approch")
 num1 = num1 + num2
 num2 = num1 - num2
 num1 = num1 - num2
-
-print("Num1 =",num1)
-print("Num2 =",num2)
+print(f'After Swapping - num1: {num1}, num2:{num2}')
 
 
 # Third Approch
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(f'Before Swapping - num1: {num1}, num2:{num2}')
+
 num1 = num1 * num2
 num2 = num1 / num2
 num1 = num1 / num2
-
-print("Num1 =",num1)
-print("Num2 =",num2)
-
+print(f'After Swapping - num1: {num1}, num2:{num2}')
