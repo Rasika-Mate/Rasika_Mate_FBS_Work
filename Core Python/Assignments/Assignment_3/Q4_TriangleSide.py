d848354d1705817d40ed1b70aa60a9a1bf9591bf
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-26 21:38:31 +0530

    Q4_TriangleSide.py

--- Core Python/Assignments/Assignment_3/Q4_TriangleSide.py
+++ Core Python/Assignments/Assignment_3/Q4_TriangleSide.py
@@ -2,10 +2,11 @@
 # Triangle Inequality Theorem: if sum of any two sides is greater than the third side then Valid Triangle.
 
 s1 = float(input("Enter First side: "))
-s2 = float(input("Enter First side: "))
-s3 = float(input("Enter First side: "))
+s2 = float(input("Enter Second side: "))
+s3 = float(input("Enter Third side: "))
 
 if(s1+s2>s3 and s1+s3>s2 and s3+s2>s1):
     print("It is Valid Triangle")
 else:
+
     print("Not Valid Triangle.")
