a07207f1bcb1628ffef24cc3d89eafc8a963a89a
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-26 21:51:41 +0530

    Q6_ProfitLoss.py

--- Core Python/Assignments/Assignment_3/Q6_ProfitLoss.py
+++ Core Python/Assignments/Assignment_3/Q6_ProfitLoss.py
@@ -3,7 +3,18 @@
 sp = int(input("Enter Selling Price: "))
 cp = int(input("Enter Cost Price: "))
 
+# Method 1
 if(sp > cp):
     print("Profit")
 else:
     print("Loss")
+
+# Method 2
+if sp > cp:
+    profit = sp - cp
+    print("Profit: ", profit)
+elif cp > sp:
+    loss = cp - sp
+    print("Loss: ", loss)
+else:
+    print("No Profit, No Loss")
