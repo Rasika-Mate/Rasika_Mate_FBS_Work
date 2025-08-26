462efbc3d5d32751e22c0e2ccfe9d921eaca694e
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-26 21:10:41 +0530

    Delete Core Python/Demos directory

--- Core Python/Demos/MembershipOp.py
+++
@@ -1,9 +0,0 @@
-# Membership: Check if value is present or not in sequence
-
-# 1. in
-print('a' in 'Demos')
-print('s' in 'Demos')
-
-# 2. not in
-print('a' not in 'Demos')
-print('s' not in 'Demos')
--- Core Python/Demos/Notes.py
+++
@@ -1,8 +0,0 @@
-amount = 5000
-
-amount = 5000 // 2000
-amount = 5000 % 2000
-
-amount = 1000 // 500
-amount = 1000 % 500
-
