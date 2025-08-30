363ad7277830ffb1aff123187eaa77c35b167a9d
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-08-30 18:43:43 +0530

    Add files via upload

---
+++ Core Python/Assignments/Assignment_3/Q12_Palindrome.py
@@ -0,0 +1,11 @@
+# 12. WAP to check if given 3 digit number is a palindrome or not.
+
+num = int(input('Enter three-digit Number: '))
+
+first = num // 100  # first digit
+last = num % 10  # last digit
+
+if first == last:
+    print(num, "is a Palindrome Number")
+else:
+    print(num, "is Not a Palindrome Number")
