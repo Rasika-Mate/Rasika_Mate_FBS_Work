96068c0ded5fb261d28e9f195d69fc2f0c760bf5
Rasika Mate <120672495+Rasika-Mate@users.noreply.github.com>
2025-09-10 18:57:15 +0530

    Delete Core Python/Assignments/Assignment_4 directory

--- Core Python/Assignments/Assignment_4/Q4_FactorialNo.py
+++
@@ -1,9 +0,0 @@
-# 4. WAP to print factorial of a number.abs
-
-num = int(input("Enter Number: "))
-i = 1
-fact = 1
-while(i <= num):
-    fact *= i
-    i += 1
-print("Factorial Number: ", fact)
--- Core Python/Assignments/Assignment_4/Q8_DivisibleBy7&Multiple5.py
+++
@@ -1,8 +0,0 @@
-# 8. WAP to find which numbers are divisible by 7 and multiple of 5 in a given range.
-
-beg = int(input("Enter beginning of Range: "))
-end = int(input("Enter Ending of Range: "))
-
-for num in range(beg, end+1):
-    if(num%7==0 and num%5==0):
-        print(num)
